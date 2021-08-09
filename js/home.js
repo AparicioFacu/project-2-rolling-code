3d0966fbbe16624b02940be8800d1bed9fa69b76
Rodrigo <rodrigomendozar10@gmail.com>
2021-08-09 16:14:25 -0300

    renderizado los elemntos(falta funcion cambiarImagen).Ahora: Crear carousel etapa 1

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 // const gamesCarousel = document.getElementById("games-carousel");
 const gamesCards = document.getElementById("games-cards");
-
+const gamesCarousel = document.getElementById("games-carousel");
 // Creo array de games para simular localStorage
 const newGamesHarcodeados = [
   {
@@ -13,8 +13,8 @@ const newGamesHarcodeados = [
     src1: "https://res.cloudinary.com/dcx1rcwvu/image/upload/v1628383101/Video%20Games/wp5117132_pvxabh.jpg",
     src2: "https://res.cloudinary.com/dcx1rcwvu/image/upload/v1628382791/Video%20Games/wallpapersden.com_dragon-ball-z-kakarot-game-poster_1920x1080_trerme.jpg",
     categoria1: "Acción",
-    categoria2: "Cooperativo",
-    categoria3: "Violencia",
+    categoria2: "Violencia",
+    categoria3: "",
     compatibilidad1: '<i class="fab fa-windows me-2">',
     compatibilidad2: '<i class="fab fa-apple"></i>',
     //datos para cards
@@ -22,6 +22,26 @@ const newGamesHarcodeados = [
     fechaLimite: "19/8/21",
     descuento: "-50%",
     nuevoPrecio: "750.00",
+  },
+  {
+    //datos para usuario
+    id: "2",
+    titulo: "Pubg",
+    precio: "500.00",
+    url: "https://res.cloudinary.com/dcx1rcwvu/image/upload/v1628178941/Video%20Games/pugb1_kwch2r.jpg",
+    // datos para carrusel
+    src1: "https://res.cloudinary.com/dcx1rcwvu/image/upload/v1628178948/Video%20Games/pugb2_wm9cuj.jpg",
+    src2: "https://res.cloudinary.com/dcx1rcwvu/image/upload/v1628179322/Video%20Games/pugb3_icejl5.jpg",
+    categoria1: "Acción",
+    categoria2: "Cooperativo",
+    categoria3: "Violencia",
+    compatibilidad1: '<i class="fab fa-windows me-2">',
+    compatibilidad2: '<i class="fab fa-apple"></i>',
+    //datos para cards
+    src: "https://res.cloudinary.com/dcx1rcwvu/image/upload/v1628534013/Video%20Games/pugb-card_kr0rfo.jpg",
+    fechaLimite: "19/8/21",
+    descuento: "-30%",
+    nuevoPrecio: "350.00",
   },
 ];
 
@@ -70,4 +90,71 @@ mostrarCardsGames();
 
 // CAROUSEL VIDEO GAMES
 
-// CREAR CAROUSEL 
+// CREAR CAROUSEL
+
+// funcion que me renderice el contenido del carousel
+function mostrarCarouselGames() {
+  const contenido = games.map((game) => {
+    // console.log(usuario.id);
+    return ` 
+    <div class="row justify-content-center">
+        <div id="main-image" class="col-10 col-md-8 game-image"
+            data-default-image="${game.url}" >
+            <button><b>BUY NOW</b></button>
+        </div>
+        <div class="d-md-flex col-10 col-md-3 game-description row">
+            <div class="mt-2 col-12">
+                <h1>${game.titulo}</h1>
+            </div>
+            <div class="d-none col-5 d-md-flex col-md-12 flex-md-column">
+                <img id="image-1"
+                    src="${game.src1}"
+                    alt="img1">
+                <img id="image-2"
+                    src="${game.src2}"
+                    alt="img2">
+            </div>
+            <div class="d-none d-md-flex">
+                <h4>Ya disponible</h4>
+            </div>
+            <div class="flex-wrap d-md-flex mt-1">
+                <span>${game.categoria1}</span>
+                <span>${game.categoria2}</span>
+                <span>${game.categoria3}</span>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <p><i>ARS$ ${game.precio}</i></p>
+                <h5><i class="fab fa-apple me-2"></i><i class="fab fa-windows"></i></h5>
+            </div>
+         </div>
+    </div> 
+       `;
+       
+  });
+  console.log(contenido.join(""));
+  const allHtmlcontent = contenido.join("");
+  gamesCarousel.innerHTML = allHtmlcontent;
+}
+mostrarCarouselGames();
+// Funcion para cambiar imagen con hover y  establecer imagen de fondo
+function cambiarImagen() {
+  const mainImage = document.getElementById("main-image");
+  const image1 = document.getElementById("image-1");
+  const image2 = document.getElementById("image-2");
+
+  mainImage.style.backgroundImage = `url(${mainImage.dataset.defaultImage})`;
+  image1.addEventListener("mouseover", () => {
+    mainImage.style.backgroundImage = `url(${image1.src})`;
+  });
+  image1.addEventListener("mouseleave", () => {
+    mainImage.style.backgroundImage = `url(${mainImage.dataset.defaultImage})`;
+  });
+  image2.addEventListener("mouseover", () => {
+    mainImage.style.backgroundImage = `url(${image2.src})`;
+  });
+  image2.addEventListener("mouseleave", () => {
+    mainImage.style.backgroundImage = `url(${mainImage.dataset.defaultImage})`;
+  });
+}
+
+cambiarImagen();
