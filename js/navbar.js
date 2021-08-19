b1fc0adaa28a527602ca2ad3bc6dbb9040b7431d
Rodrigo <rodrigomendozar10@gmail.com>
2021-08-19 12:12:01 -0300

    Admin board estilo arreglado + Nombre Usuario en nadbar

--- js/navbar.js
+++ js/navbar.js
@@ -4,6 +4,7 @@
 const sidebar = document.querySelector(".sidebar");
 const botonCerrar = document.getElementById("btn");
 const botonBuscar = document.getElementById("buscador");
+const usuarioLogueado = document.getElementById("usuario-logeado");
 
 botonCerrar.addEventListener("click", () => {
   sidebar.classList.toggle("open");
@@ -12,3 +13,18 @@ botonCerrar.addEventListener("click", () => {
 botonBuscar.addEventListener("click", () => {
   sidebar.classList.toggle("open");
 });
+
+let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+const notaJson = JSON.stringify(usuarios);
+localStorage.setItem('usuarios', notaJson);
+console.log('usuarios', usuarios);
+
+function mostrarName(id){
+  let usuarioActual = usuarios[usuarios.length - 1];
+  console.log('usuarioactual',usuarioActual);
+  const contenido = `
+   Hola ${usuarioActual.fullname} 
+`;
+usuarioLogueado.innerHTML = contenido;
+}
+mostrarName();
