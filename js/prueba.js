5e6feec90055f7386f6d32946fa380628cadcd4f
Rodrigo <rodrigomendozar10@gmail.com>
2021-08-19 19:59:07 -0300

    listo

--- js/prueba.js
+++
@@ -1,8 +0,0 @@
-const url = localStorage.getItem("recent-image");
-console.log(url);
-
-const imagenPrueba = document.getElementById("img-test");
-
-setTimeout(()=> {
-    imagenPrueba.src= url;
-}, 1500)
